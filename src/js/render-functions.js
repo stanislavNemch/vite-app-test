a790f3faa9bd70a126619d0dca19810e0379b6cf
Stanislav Nemchenko <stas.buka@gmail.com>
2025-06-26 18:46:46 +0300

    first commit

---
+++ src/js/render-functions.js
@@ -0,0 +1,5 @@
+// Импортируем функцию для получения фонового изображения
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
---
+++ vite.config.js
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+import viteCompression from 'vite-plugin-compression';
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('src/**/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240, // Compress files larger than 10KB
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
+});
